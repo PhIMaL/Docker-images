c9c6de6cf07e1582ddda754be36a3e69077d8109
Gert-Jan <gert-jan.both@cri-paris.com>
2019-07-02 11:46:31 +0200

    Initial commit

---
+++ start_notebook_gpu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#First automatically find project directory
+cd ../
+projectdir=$(pwd)
+cd config/
+
+# Make docker-sync for project
+sed "s#{{PROJECT_FOLDER}}#$projectdir#g" docker_templates/docker-sync_template.yml > docker-sync.yml
+
+#Make docker-compose file dependent for cpu
+sed "s#{{IMAGE}}#phimal/gpu:latest#g" docker_templates/docker-compose_template.yml > docker-compose.yml
+
+# Run docker sync 
+sudo docker-sync-stack start
