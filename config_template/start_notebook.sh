8c9f622f80d69500befed8a451da70633d5063c2
Gert-Jan <gertjanboth@gmail.com>
2019-07-08 12:11:56 +0200

    No more docker-sync!

--- config_template/start_notebook.sh
+++ config_template/start_notebook.sh
@@ -5,17 +5,19 @@ cd ../
 projectdir=$(pwd)
 cd config/
 
-# Make docker-sync for project
-sed "s#{{PROJECT_FOLDER}}#$projectdir#g" docker_sync_templates/docker-sync_template.yml > docker-sync.yml
-
 if hash nvidia-docker 2>/dev/null; then
-        sed "s#{{IMAGE}}#[PASTE DOCKER IMAGE NAME HERE]#g" docker_sync_templates/docker-compose_gpu.yml > docker-compose.yml
+        sed "s#{{IMAGE}}#phimal/projects:diffusioncharacterization#g" docker_sync_templates/docker-compose_gpu.yml > docker-compose.yml
     else
-        sed "s#{{IMAGE}}#[PASTE DOCKER IMAGE NAME HERE]#g" docker_sync_templates/docker-compose_cpu.yml > docker-compose.yml
+        sed "s#{{IMAGE}}#phimal/projects:diffusioncharacterization#g" docker_sync_templates/docker-compose_cpu.yml > docker-compose.yml
     fi
 
-# Clean old docker-sync
-sudo docker-sync-stack clean
 
-# Run docker sync 
-sudo docker-sync-stack start
+# Setting mounting directory
+sed -i "s#{{PROJECTDIR}}#$projectdir#g" docker-compose.yml
+
+if [ ! "$(docker ps -a -f name=config_compute_1)" ]; then
+    docker-compose restart config_compute_1
+   else
+    docker-compose up -d
+   fi
+
